ca101271648ea82e47b4e18280aebbc4a5d96cdd
Nikhil Chandan <nikhil.chandan1@gmail.com>
2020-03-06 14:18:32 +0530

    Add files via upload

--- test/employee_test.js
+++ test/employee_test.js
@@ -1,48 +1,81 @@
-
 //Require the dev-dependencies
-const chai           = require('chai');
-const chaiHttp       = require('chai-http');
-const app         = require('../app');
-const should         = chai.should();
-const expect         = chai.expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const should = chai.should();
+const expect = chai.expect;
 
 
 chai.use(require('chai-json-schema'));
 chai.use(chaiHttp);
 
 //get all emp
-  describe('/GET all Employee:', () => {
+describe('/GET all Employee:', () => {
     it('it expect to GET all the Employee', (done) => {
-      chai.request(app)
-          .get('/api/employees')
-          .end((err, res) => {
-              expect(res).to.have.status(200);
-              expect(res).to.be.json;
-              expect(res.body).to.be.an('array');
-            done();
-          });
+        chai.request(app)
+            .get('/api/employees')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('array');
+                done();
+            });
     });
 });
 
+//get emp by id
+describe('/GET Employee by id:', () => {
+    var empId = 1;
+    it('it expect to GET all the Employee', (done) => {
+        chai.request(app)
+            .get('/api/employees/' + empId)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('array');
+                done();
+            });
+    });
+});
 
+//post an employee
 describe('/POST Employee', () => {
-    // with error, pages is required
     it('Adding an employee', (done) => {
-      let employee = { 
-        "firstName": "sp12321",
-        "address": "mumbai",
-        "mobileNo": "9876543210",
-        "designation": "Software Eng.",
-        "location": "India",
-        "isDeleted": "false"
-    }
-      chai.request(app)
-        .post('/api/employees')
-        .send( employee )
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.an('object');
-          done();
-        });
+        let employee = {
+            "firstName": "sp12321",
+            "address": "mumbai",
+            "mobileNo": "9876543210",
+            "designation": "Software Eng.",
+            "location": "India",
+            "isDeleted": "false"
+        }
+        chai.request(app)
+            .post('/api/employees')
+            .send(employee)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.an('object');
+                done();
+            });
     });
 });
+
+//patch an employee
+describe('/Patch Employee', () => {
+    it('Adding an employee', (done) => {
+        let empId = 1;
+        let employee = {
+            "firstName": "wertyu",
+        }
+        chai.request(app)
+            .patch('/api/employees/' + empId)
+            .send(employee)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.an('object');
+                done();
+            });
+    });
+});
+
+
